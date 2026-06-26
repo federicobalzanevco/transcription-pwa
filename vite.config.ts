2e2252a96f739bf56a06a4b4ea99b9615c604f9f
fbalzan_L5vBP <federico.balzan@evco.it>
2026-06-26 15:46:46 +0200

    previous

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/transcription-pwa/',
   plugins: [react(), VitePWA({
+    base: '/transcription-pwa/',
     registerType: 'prompt',
     injectRegister: false,
 
